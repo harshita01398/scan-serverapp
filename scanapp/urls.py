202aead8dd7158b4a96fb9ececa75b0c7cab84f1
Harshita <harshita01398@gmail.com>
2020-05-15 02:07:23 +0530

    IP matching
    Signed-off-by: Harshita <harshita01398@gmail.com>

--- scanapp/urls.py
+++ scanapp/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('', views.signIn, name='index'),
-    path('demo', views.demo, name='demo'),
-    path('check_ip', views.check_ip, name='check_ip'),
-    path('get_ip', views.get_ip, name='get_ip'),
+    path('receive', views.receive, name='receive'),
     path('connect', views.connect, name='connect'),
    ]
--- scanapp/views.py
+++ scanapp/views.py
@@ -1,90 +1,67 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from ipware import get_client_ip
 
-name = ""
-passw = ""
-ip = ""
+hotspot_name = ""
+password = ""
+scanned_ip = ""
 
 @csrf_exempt
-def demo(request):
+def receive(request):
 
-    global name, passw, ip
+    global hotspot_name, password, scanned_ip
 
-    name = request.POST.get('name')
-    passw = request.POST.get('pass')
-    ip = request.POST.get('ip')
+    hotspot_name = request.POST.get('name')
+    password = request.POST.get('pass')
+    scanned_ip = request.POST.get('ip')
+
+    if str(hotspot_name) == "" or str(scanned_ip) == "":
+
+        response = {
+            'status' : "false",
+            'message' : "Invalid request. Please check."
+        }
+
+        return JsonResponse(response, status = 500)
     
-    
-    print(name)
-    print(passw)
-    print(ip)
-    
-    data = {
-        'msg': "success"
-    }
-    return JsonResponse(data)
+    else:
+
+        print(hotspot_name)
+        print(password)
+        print(scanned_ip)
+        
+        response = {
+            'message' : "Request received."
+        }
+        return JsonResponse(response)
 
 def connect(request):
 
-    global name, passw, ip
-    print(ip)
+    global hotspot_name, password, scanned_ip
 
-    if ip == "":
-        print("no ip")
-        return JsonResponse(None)
+    current_ip = request.GET.get('my_ip')
+
+    if scanned_ip == "" or str(scanned_ip) != str(current_ip):
+        print("Waiting for device to connect.")
+
+        response = {
+            'status' : "false",
+            'message' : "No requesting device."
+        }
+
+        return JsonResponse(response, status = 500)
     
     else:
-        data = {
-        'name': name,
-        'pass': passw
+        print("IP matched!")
+
+        response = {
+        'hotspot_name' : hotspot_name,
+        'pass' : password
         }
-        name = ""
-        passw = ""
-        ip = ""
 
-        return JsonResponse(data)
+        hotspot_name = ""
+        password = ""
+        scanned_ip = ""
 
-def check_ip(request):
-    client_ip, is_routable= get_client_ip(request)
-    if client_ip is None:
-        print("Unable to get the client's IP address")
-    else:
+        return JsonResponse(response)
 
-        # We got the client's IP address
-        # if is_routable:
-        #     print("Routable :  " + str(client_ip))
-        #     print(client_ip)
-        # else:
-        #     print(client_ip)
-
-        data = {
-            'ip': client_ip,
-            # 'realip': rip
-        }
-        return JsonResponse(data)
-
-def get_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        print("returning FORWARDED_FOR")
-        ip = x_forwarded_for.split(',')[-1].strip()
-    elif request.META.get('HTTP_X_REAL_IP'):
-        print("returning REAL_IP")
-        ip = request.META.get('HTTP_X_REAL_IP')
-    else:
-        print("returning REMOTE_ADDR")
-        ip = request.META.get('REMOTE_ADDR')
-    data = {
-        'ip': ip
-            # 'realip': rip
-    }
-    return JsonResponse(data)
-
-
-    
-
-
-
-# Create your views here.
