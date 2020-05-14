8eec6453b2bc8538e929a34e7435fbb3287beb71
Harshita <harshita01398@gmail.com>
2020-05-15 03:27:49 +0530

    error conditions handled
    Signed-off-by: Harshita <harshita01398@gmail.com>

--- scanapp/views.py
+++ scanapp/views.py
@@ -5,26 +5,38 @@ from django.views.decorators.csrf import csrf_exempt
 hotspot_name = ""
 password = ""
 scanned_ip = ""
+request_valid = False
 
 @csrf_exempt
 def receive(request):
 
-    global hotspot_name, password, scanned_ip
+    global hotspot_name, password, scanned_ip, request_valid
 
     hotspot_name = request.POST.get('name')
     password = request.POST.get('pass')
     scanned_ip = request.POST.get('ip')
 
-    if str(hotspot_name) == "" or str(scanned_ip) == "":
+    if hotspot_name == None or password == None or scanned_ip == None:
 
         response = {
             'status' : "false",
-            'message' : "Invalid request. Please check."
+            'message' : "Invalid request. Please check for missing parameters."
+        }
+
+        return JsonResponse(response, status = 500)
+    
+    elif hotspot_name == "" or scanned_ip == "" :
+
+        response = {
+            'status' : "false",
+            'message' : "Invalid request. Please check value of hotspot name and/or IP address."
         }
 
         return JsonResponse(response, status = 500)
     
     else:
+
+        request_valid = True
 
         print(hotspot_name)
         print(password)
@@ -37,11 +49,11 @@ def receive(request):
 
 def connect(request):
 
-    global hotspot_name, password, scanned_ip
+    global hotspot_name, password, scanned_ip, request_valid
 
     current_ip = request.GET.get('my_ip')
 
-    if scanned_ip == "" or str(scanned_ip) != str(current_ip):
+    if not request_valid or scanned_ip != current_ip:
         print("Waiting for device to connect.")
 
         response = {
@@ -59,9 +71,7 @@ def connect(request):
         'pass' : password
         }
 
-        hotspot_name = ""
-        password = ""
-        scanned_ip = ""
+        request_valid = False
 
         return JsonResponse(response)
 
