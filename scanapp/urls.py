773d9c5dc341a2bf53c07994003c355e0a7ce76f
Harshita <harshita01398@gmail.com>
2020-05-20 22:27:48 +0530

    Status check files added
    Signed-off-by: Harshita <harshita01398@gmail.com>

--- scanapp/urls.py
+++ scanapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     # path('', views.signIn, name='index'),
     path('receive', views.receive, name='receive'),
     path('connect', views.connect, name='connect'),
+    path('bike_status', views.bike_status, name='bike_status'),
+    path('android_status', views.android_status, name='android_status')
    ]
--- scanapp/views.py
+++ scanapp/views.py
@@ -4,19 +4,23 @@ from django.views.decorators.csrf import csrf_exempt
 
 hotspot_name = ""
 password = ""
-scanned_ip = ""
+bike_ip = ""
+android_ip = ""
+
+status = True
 request_valid = False
 
 @csrf_exempt
 def receive(request):
 
-    global hotspot_name, password, scanned_ip, request_valid
+    global hotspot_name, password, bike_ip, android_ip, request_valid
 
     hotspot_name = request.POST.get('name')
     password = request.POST.get('pass')
-    scanned_ip = request.POST.get('ip')
+    bike_ip = request.POST.get('bikeIP')
+    android_ip = request.POST.get('androidIP')
 
-    if hotspot_name == None or password == None or scanned_ip == None:
+    if hotspot_name==None or password==None or bike_ip==None or android_ip==None:
 
         response = {
             'status' : "false",
@@ -25,11 +29,11 @@ def receive(request):
 
         return JsonResponse(response, status = 500)
     
-    elif hotspot_name == "" or scanned_ip == "" :
+    elif hotspot_name=="" or bike_ip=="" or android_ip=="":
 
         response = {
             'status' : "false",
-            'message' : "Invalid request. Please check value of hotspot name and/or IP address."
+            'message' : "Error. Please check value of hotspot name and/or IP address."
         }
 
         return JsonResponse(response, status = 500)
@@ -40,7 +44,8 @@ def receive(request):
 
         print(hotspot_name)
         print(password)
-        print(scanned_ip)
+        print(bike_ip)
+        print(android_ip)
         
         response = {
             'message' : "Request received."
@@ -49,11 +54,11 @@ def receive(request):
 
 def connect(request):
 
-    global hotspot_name, password, scanned_ip, request_valid
+    global hotspot_name, password, bike_ip, android_ip, request_valid
 
-    current_ip = request.GET.get('my_ip')
+    current_ip = request.GET.get('bikeIP')
 
-    if not request_valid or scanned_ip != current_ip:
+    if not request_valid or bike_ip!=current_ip:
         print("Waiting for device to connect.")
 
         response = {
@@ -68,10 +73,48 @@ def connect(request):
 
         response = {
         'hotspot_name' : hotspot_name,
-        'pass' : password
+        'pass' : password,
+        'androidIP' : android_ip
         }
 
         request_valid = False
 
+        return JsonResponse(response)
+
+def bike_status(request):
+
+    global android_ip, status
+
+    android_ip = request.GET.get('androidIP')
+    status = False
+
+    response = {
+            'message' : "Request received."
+        }
+    return JsonResponse(response)
+
+
+def android_status(request):
+
+    global android_ip, status
+
+    current_ip = request.GET.get('androidIP')
+    print("android checking status")
+
+    if status or current_ip!=android_ip:
+        print("No status")
+
+        response = {
+            'status' : "false",
+            'message' : "Error. Can't get status"
+        }
+        return JsonResponse(response, status=500)
+
+    else:
+        print("Bike can't connect.")
+        response = {
+            'message' : "Bike Can't connect"
+        }
+        status = True
         return JsonResponse(response)
 
